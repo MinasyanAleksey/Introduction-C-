190599e02be2f0428ed4af6a4cadeccbaccfe8a8
Alexey Minasyan <mr.minasyan@list.ru>
2022-08-18 18:43:25 +0300

    Created folder HomeWork

---
+++ HomeWorks/HomeWork 1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*int a = 5;
+int b = 7;
+
+Console.WriteLine($"Даны два числа: {a} и {b}");*/
+
+//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 > number2)
+{
+    Console.WriteLine($"Число {number1} больше числа {number2}");
+}
+else
+{
+    Console.WriteLine($"Число {number1} меньше числа {number2}");
+}
