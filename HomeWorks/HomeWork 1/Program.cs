9bb237830799da45289a35cfebc5e16fba724a55
Alexey Minasyan <mr.minasyan@list.ru>
2022-08-18 19:15:37 +0300

    Created third task

--- HomeWorks/HomeWork 1/Program.cs
+++ HomeWorks/HomeWork 1/Program.cs
@@ -1,6 +1,6 @@
-﻿/*//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-Console.WriteLine("Введите первое число: ");
+/*Console.WriteLine("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите второе число: ");
@@ -17,7 +17,7 @@ else
 
 //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.WriteLine("Введите первое число: ");
+/*Console.WriteLine("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите второе число: ");
@@ -31,4 +31,18 @@ int max = number1;
 if(number2 > max) max = number2;
 if(number3 > max) max = number3;
 
-Console.WriteLine($"Максимальное число - {max}");
+Console.WriteLine($"Максимальное число - {max}");*/
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    Console.WriteLine($"Число {number} является четным");
+}
+else
+{
+    Console.WriteLine($"Число {number} не является четным");
+}
