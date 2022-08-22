1dbca2457b905dfa24bc4a7a83344ae4c3ad92af
Alexey Minasyan <mr.minasyan@list.ru>
2022-08-22 17:58:41 +0300

    Создал папку Второго задания и решил первую задачу

---
+++ HomeWorks/HomeWork2/Program.cs
@@ -0,0 +1,16 @@
+﻿//ЗАДАЧА 1 - Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int FindSecondNumber(int number)
+{
+    int left = number / 10;
+    //int right = number % 10;
+
+    int result = left % 10;
+
+    return result;
+}
+
+int randNumber = new Random().Next(100,1000);
+int metod = FindSecondNumber(randNumber);
+Console.WriteLine($"Случайное трехзначное число - {randNumber}");
+Console.WriteLine($"Результат задачи - {metod}");
