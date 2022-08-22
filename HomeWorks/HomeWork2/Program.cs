c3a6b6c08b471c76e88218cbdae78983f204417d
Alexey Minasyan <mr.minasyan@list.ru>
2022-08-22 18:01:46 +0300

    Решил вторую задачу

--- HomeWorks/HomeWork2/Program.cs
+++ HomeWorks/HomeWork2/Program.cs
@@ -1,6 +1,6 @@
 ﻿//ЗАДАЧА 1 - Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-int FindSecondNumber(int number)
+/*int FindSecondNumber(int number)
 {
     int left = number / 10;
     //int right = number % 10;
@@ -13,4 +13,22 @@ int FindSecondNumber(int number)
 int randNumber = new Random().Next(100,1000);
 int metod = FindSecondNumber(randNumber);
 Console.WriteLine($"Случайное трехзначное число - {randNumber}");
-Console.WriteLine($"Результат задачи - {metod}");
+Console.WriteLine($"Результат задачи - {metod}");*/
+
+
+//ЗАДАЧА 2 - Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+void ThirdDigit(int number)
+{
+    string numberText = Convert.ToString(number);
+
+    if (numberText.Length > 2)
+        Console.WriteLine("Третья цифра - " + numberText[2]);
+    else
+        Console.WriteLine("Третьей цифры нет");
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+ThirdDigit(num);
