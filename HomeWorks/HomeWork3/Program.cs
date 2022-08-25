9c8af08ab78549521aa96d548eee57ab80427200
Alexey Minasyan <mr.minasyan@list.ru>
2022-08-25 19:57:13 +0300

    Создал папку HomeWork3 и добавил 1-й вариант решения 19 задачи

---
+++ HomeWorks/HomeWork3/Program.cs
@@ -0,0 +1,31 @@
+﻿//ДОМАШНЕЕ ЗАДАНИЕ №3
+
+//Задача 19 - Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// ------ВАРИАНТ 1------
+
+void Polindrom(int num)
+{
+    int current = num;
+    int result = 0;
+    int i = 100000;
+
+    while (num > 0)
+    {
+        int a = num % 10;
+        num = num / 10;
+        result = result + a * (i / 10);
+        i = i / 10;
+    }
+    
+    if(current == result)
+        Console.WriteLine($"Число {current} - является палиндромом");
+    else
+        Console.WriteLine($"Число {current} - не является палиндромом");
+
+}
+
+Console.Write("Введите любое пятизначое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Polindrom(number);
