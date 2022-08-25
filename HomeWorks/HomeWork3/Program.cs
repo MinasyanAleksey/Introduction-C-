a1f32541aba879f27a0b49c6e7eabec77303af77
Alexey Minasyan <mr.minasyan@list.ru>
2022-08-25 20:04:54 +0300

    добавил решение 21 задачи

--- HomeWorks/HomeWork3/Program.cs
+++ HomeWorks/HomeWork3/Program.cs
@@ -65,7 +65,7 @@ Polindrom(number);*/
 
 // ------ВАРИАНТ 3------ Работает с любыми числами
 
-void Polindrom(long num)
+/*void Polindrom(long num)
 {
     string numberText = Convert.ToString(num);
     long length = numberText.Length;
@@ -92,4 +92,28 @@ void Polindrom(long num)
 Console.Write("Введите любое число: ");
 long number = Convert.ToInt64(Console.ReadLine());
 
-Polindrom(number);
+Polindrom(number);*/
+
+
+// Задача 21 - Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double FindDistance(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double n1 = x2 - x1;
+    double n2 = y2 - y1;
+    double n3 = z2 - z1;
+    return Math.Sqrt(n1*n1 + n2*n2 + n3*n3);
+}
+
+Console.WriteLine("Введите координаты первой точки (x + enter, y + enter, z + enter):");
+double xA = Convert.ToDouble(Console.ReadLine());
+double yA = Convert.ToDouble(Console.ReadLine());
+double zA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки (x + enter, y + enter, z + enter):");
+double xB = Convert.ToDouble(Console.ReadLine());
+double yB = Convert.ToDouble(Console.ReadLine());
+double zB = Convert.ToDouble(Console.ReadLine());
+
+double dist = FindDistance(xA, yA, zA, xB, yB, zB);
+Console.WriteLine($"Расстояние между точками - {dist}");
