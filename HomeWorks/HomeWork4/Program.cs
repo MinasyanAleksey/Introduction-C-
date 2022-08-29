fe26fb64fce5e3a34387cfa200b124dc851c1477
Alexey Minasyan <mr.minasyan@list.ru>
2022-08-29 19:50:38 +0300

    добавил решение задачи 27

--- HomeWorks/HomeWork4/Program.cs
+++ HomeWorks/HomeWork4/Program.cs
@@ -2,7 +2,7 @@
 
 // Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-void FindLevel(int num1, int num2)
+/*void FindLevel(int num1, int num2)
 {
     int res = 1;
     for(int current = 1; current <= num2; current++)
@@ -17,4 +17,27 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-FindLevel(number1, number2);
+FindLevel(number1, number2);*/
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+void SumOfNumbers(int num)
+{
+    int n = num;
+    int sum = 0;
+    
+    while(num > 0)
+    {
+        sum = sum + num % 10;
+        num /= 10;
+    }
+
+    Console.WriteLine($"Сумма цифр числа {n} = {sum}");
+    
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+SumOfNumbers(number);
