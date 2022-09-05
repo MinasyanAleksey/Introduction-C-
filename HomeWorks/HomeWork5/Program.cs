5a183ab99a9dbcffb8181de87a32af8fb11c35cc
Alexey Minasyan <mr.minasyan@list.ru>
2022-09-05 18:50:50 +0300

    добавил решение задачи №38

--- HomeWorks/HomeWork5/Program.cs
+++ HomeWorks/HomeWork5/Program.cs
@@ -45,7 +45,7 @@ Console.WriteLine($"Колличество четных чисел в масси
 
 //Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
-int[] CreateRadomArray(int size, int minValue, int maxValue)
+/*int[] CreateRadomArray(int size, int minValue, int maxValue)
 {
     int[] newArray = new int[size];
 
@@ -82,4 +82,56 @@ int max = Convert.ToInt32(Console.ReadLine());
 int[] myArray = CreateRadomArray(size, min, max);
 
 ShowArray(myArray);
-Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях - {SumOfElements(myArray)}");
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях - {SumOfElements(myArray)}");*/
+
+
+//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] CreateRadomArray(int size, int minValue, int maxValue)
+{
+    double [] newArray = new double[size];
+    Random rand = new Random ();
+    
+    for(int i = 0; i < size; i++)
+        newArray[i] = Math.Round((rand.NextDouble() + rand.Next(minValue, maxValue + 1)), 2);
+    
+    return newArray;
+}
+
+double FindSum(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+        
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > max) max = array[i];
+        else
+            if(array[i] < min) min = array[i];
+    }
+
+    double result = Math.Round(max - min, 2);
+    return result;
+}
+
+void ShowArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимально возможное значение: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимально возможное значение: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+double[] myArray = CreateRadomArray(size, min, max);
+
+ShowArray(myArray);
+
+Console.Write($"Разница между максимальным и минимальным элементами массива = {FindSum(myArray)}");
