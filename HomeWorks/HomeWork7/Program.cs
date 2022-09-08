df7606a6cc0c7fe7df1cdbcaa6aab4d6ee21b386
Alexey Minasyan <mr.minasyan@list.ru>
2022-09-08 20:41:17 +0300

    Добавил папку HomeWork7 и решение задачи №47

---
+++ HomeWorks/HomeWork7/Program.cs
@@ -0,0 +1,47 @@
+﻿//-----------------------------------------
+// ----------- ДОМАШНЯЯ РАБОТА 7 ----------
+//-----------------------------------------
+
+//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double [,] CreateRandome2dArray()
+{
+    Console.Write("Введите количество строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите количество столбцов: ");
+    int colums = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите минимально возможное значение: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите максимально возможное значение: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    double[,] newArray = new double[rows, colums];
+    Random rand = new Random ();
+
+    for(int i = 0; i < rows; i++)
+    {   for(int j = 0; j < colums; j++)
+        {
+            newArray[i,j] = Math.Round((rand.NextDouble() + rand.Next(minValue, maxValue + 1)), 2);
+        }
+    }       
+    return newArray;
+}
+
+void Show2dArray(double[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
+
+double[,] newArray = CreateRandome2dArray();
+Show2dArray(newArray);
